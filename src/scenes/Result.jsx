04863f975b0c163a26bbd4307abf1630bf0b21e9
Vibin P <96070519+vbn-P@users.noreply.github.com>
2023-06-18 11:18:16 +0530

    Update Result.jsx

--- src/scenes/Result.jsx
+++ src/scenes/Result.jsx
@@ -7,6 +7,7 @@ import { useSelector } from "react-redux";
 import { Paper, Stack, Title, Text, Group, Divider, Space, Button  } from '@mantine/core';
 import { useNavigate } from "react-router-dom"
 
+
 function Result({ selSymptoms, model, setSelSymptoms }) {
   const navigate = useNavigate()
   const [disease, setDisease] = useState("");
@@ -235,8 +236,141 @@ function Result({ selSymptoms, model, setSelSymptoms }) {
   [40, "Hepatitis A is a viral infection that affects the liver. It is caused by the Hepatitis A virus (HAV) and is spread through contaminated food or water, or close contact with an infected person. The symptoms of Hepatitis A can include fatigue, nausea, vomiting, abdominal pain, dark urine, and jaundice."]
   ]);
 
+  
 
-  const symptomsFinal = ['itching', 'skin_rash', 'continuous_sneezing', 'chills', 'joint_pain', 'stomach_pain', 'acidity', 'vomiting', 'burning_micturition', 'fatigue', 'mood_swings', 'weight_loss', 'restlessness', 'lethargy', 'cough', 'high_fever', 'breathlessness', 'sweating', 'indigestion', 'headache', 'yellowish_skin', 'dark_urine', 'nausea', 'loss_of_appetite', 'back_pain', 'constipation', 'abdominal_pain', 'diarrhoea', 'mild_fever', 'yellowing_of_eyes', 'swelled_lymph_nodes', 'malaise', 'blurred_and_distorted_vision', 'phlegm', 'chest_pain', 'fast_heart_rate', 'neck_pain', 'dizziness', 'obesity', 'excessive_hunger', 'muscle_weakness', 'stiff_neck', 'swelling_joints', 'loss_of_balance', 'depression', 'irritability', 'muscle_pain', 'red_spots_over_body', 'family_history'];
+  const symptomsFinal =[
+    'itching',
+    'skin_rash',
+    'nodal_skin_eruptions',
+    'continuous_sneezing',
+    'shivering',
+    'chills',
+    'joint_pain',
+    'stomach_pain',
+    'acidity',
+    'ulcers_on_tongue',
+    'muscle_wasting',
+    'vomiting',
+    'burning_micturition',
+    'spotting_urination',
+    'fatigue',
+    'weight_gain',
+    'anxiety',
+    'cold_hands_and_feets',
+    'mood_swings',
+    'weight_loss',
+    'restlessness',
+    'lethargy',
+    'patches_in_throat',
+    'irregular_sugar_level',
+    'cough',
+    'high_fever',
+    'sunken_eyes',
+    'breathlessness',
+    'sweating',
+    'dehydration',
+    'indigestion',
+    'headache',
+    'yellowish_skin',
+    'dark_urine',
+    'nausea',
+    'loss_of_appetite',
+    'pain_behind_the_eyes',
+    'back_pain',
+    'constipation',
+    'abdominal_pain',
+    'diarrhoea',
+    'mild_fever',
+    'yellow_urine',
+    'yellowing_of_eyes',
+    'acute_liver_failure',
+    'fluid_overload',
+    'swelling_of_stomach',
+    'swelled_lymph_nodes',
+    'malaise',
+    'blurred_and_distorted_vision',
+    'phlegm',
+    'throat_irritation',
+    'redness_of_eyes',
+    'sinus_pressure',
+    'runny_nose',
+    'congestion',
+    'chest_pain',
+    'weakness_in_limbs',
+    'fast_heart_rate',
+    'pain_during_bowel_movements',
+    'pain_in_anal_region',
+    'bloody_stool',
+    'irritation_in_anus',
+    'neck_pain',
+    'dizziness',
+    'cramps',
+    'bruising',
+    'obesity',
+    'swollen_legs',
+    'swollen_blood_vessels',
+    'puffy_face_and_eyes',
+    'enlarged_thyroid',
+    'brittle_nails',
+    'swollen_extremeties',
+    'excessive_hunger',
+    'extra_marital_contacts',
+    'drying_and_tingling_lips',
+    'slurred_speech',
+    'knee_pain',
+    'hip_joint_pain',
+    'muscle_weakness',
+    'stiff_neck',
+    'swelling_joints',
+    'movement_stiffness',
+    'spinning_movements',
+    'loss_of_balance',
+    'unsteadiness',
+    'weakness_of_one_body_side',
+    'loss_of_smell',
+    'bladder_discomfort',
+    'foul_smell_of urine',
+    'continuous_feel_of_urine',
+    'passage_of_gases',
+    'internal_itching',
+    'toxic_look_typhos',
+    'depression',
+    'irritability',
+    'muscle_pain',
+    'altered_sensorium',
+    'red_spots_over_body',
+    'belly_pain',
+    'abnormal_menstruation',
+    'dischromic_patches',
+    'watering_from_eyes',
+    'increased_appetite',
+    'polyuria',
+    'family_history',
+    'mucoid_sputum',
+    'rusty_sputum',
+    'lack_of_concentration',
+    'visual_disturbances',
+    'receiving_blood_transfusion',
+    'receiving_unsterile_injections',
+    'coma',
+    'stomach_bleeding',
+    'distention_of_abdomen',
+    'history_of_alcohol_consumption',
+    'fluid_over',
+    'blood_in_sputum',
+    'prominent_veins_on_calf',
+    'palpitations',
+    'painful_walking',
+    'pus_filled_pimples',
+    'blackheads',
+    'scurring',
+    'skin_peeling',
+    'silver_like_dusting',
+    'small_dents_in_nails',
+    'inflammatory_nails',
+    'blister',
+    'red_sore_around_nose',
+    'yellow_crust_ooze'];
   var percentage =0
   async function predict() {
     selSymptoms.forEach(element => {
@@ -251,7 +385,8 @@ function Result({ selSymptoms, model, setSelSymptoms }) {
         inputData.push(parseInt(allSymptoms[i]));
       }
     }
-    const reshapedTensor = tf.tensor(inputData, [1, 49]);
+    
+    const reshapedTensor = tf.tensor(inputData, [1, 132]);
 
     const output = model.predict(reshapedTensor);
    await output.array().then((result) => {
@@ -276,7 +411,7 @@ function Result({ selSymptoms, model, setSelSymptoms }) {
 
     setDisease(diseaseName);
     setDetail(detailMap.get(label))
-    if (user !== "guest") {
+    if (user !== "guest" && percent > 80) {
       const docRef = await addDoc(collection(db, user), {
         symptoms: selSymptoms,
         disease: diseaseName,
@@ -286,6 +421,7 @@ function Result({ selSymptoms, model, setSelSymptoms }) {
 
   }
 
+  console.log(disease)
 
 
   useEffect(() => {
